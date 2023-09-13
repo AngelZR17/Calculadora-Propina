3286772a5410e1b41fefbb5eca2810d23c50189d
AngelZR17 <S20120166@alumnos.itsur.edu.mx>
2023-09-12 21:19:59 -0600

    Paso 5 - Listo

--- tip-calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ tip-calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,6 +15,7 @@
  */
 package com.example.tiptime
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -30,6 +31,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -95,11 +98,12 @@ fun TipTimeLayoutPreview() {
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    val amountInput = "0"
+    var amountInput = mutableStateOf("0")
     TextField(
-        value = amountInput,
+        value = amountInput.value,
         onValueChange = {},
         modifier = modifier
     )
