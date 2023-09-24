81162caed0ef28ba22b2a7d3185bddecf57d3672
AngelDZR <S20120166@alumnos.itsur.edu.mx>
2023-09-23 20:38:49 -0700

    modificacion de la clase type.tk

--- superheroes/app/src/main/java/com/angel/superheroes/ui/theme/Type.kt
+++ superheroes/app/src/main/java/com/angel/superheroes/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.angel.superheroes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.angel.superheroes.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,6 +17,37 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+)
+
+val Cabin = FontFamily(
+    Font(R.font.Cabin_Regular, FontWeight.Normal),
+    Font(R.font.Cabin_Bold, FontWeight.Bold)
+)
+
+val Typography2=Typography(
+    bodyLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    )
+)
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +64,3 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
-)
